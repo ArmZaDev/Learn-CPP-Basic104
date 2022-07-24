091c2f1fac9512bd40616cf2d2fea84fb9fd9896
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-07-24 13:22:17 +0700

    Constant

--- test.cpp
+++ test.cpp
@@ -2,46 +2,14 @@
 
 int main(){
 
-    //integer (whole number)
-    int age = 21;
-    int year = 2023;
-    int days = 7.5;
+    // The const keyword specifies that a variable's value is constant
+    // tells the compiler to prevent anything from modifying it
+    // (read-only)
 
-    //double (number including decimal)
-    double price = 10.99;
-    double gpa = 2.5;
-    double temperature = 25.1;
-
-    //single character
-    char grade = 'A';
-    char initial = 'C';
-    char currency = '$';
-
-    //boolean (true or false)
-    bool student = false;
-    bool power = true;
-    bool forSale = true;
-
-    //string (objects that represents a sequence of text)
-    std::string name = "Fubuki";
-    std::string day = "Friday";
-    std::string food = "pizza";
-    std::string address = "123 Fake St.";
-
-    std::cout << "Hello " << name << "\n";
-    std::cout << "You are " << age << " years old\n";
-
-
-    /*
-    int x; //declaration
-    x = 5;
-    int y = 6;
-    int sum = x + y;
-
-    std::cout << x << '\n';
-    std::cout << y << '\n';
-    std::cout << sum << '\n';
-    */
+    const double pi = 3.14159;
+    const int LIGHT_SPEED = 299792458;
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
 
     return 0;
 }
