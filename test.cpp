978013b3f99a2fb7df80aaf1a25fe2794b5e78ba
ArmZaDev <arztv001@gmail.com>
2022-07-24 13:51:32 +0700

    Arithmetic operators

--- test.cpp
+++ test.cpp
@@ -1,26 +1,33 @@
 #include <iostream>
-#include <vector>
-
-//typedef std::vector<std::pair<std::string, int>> pairlist_t;
-//typedef std::string text_t;
-//typedef int number_t;
-using text_t = std::string;
-using number_t = int;
 
 int main(){
 
-    // typedef = reserved keyword used to create an additional name
-    //           (alias) for another data type.
-    //           New identifier for an exitsting type
-    //           Helps with readability and reduces typos
-    //           Use when there is a clear benefit
-    //           Replaced with 'using' (work better w/ templates)
+    // arithimetic operatoes = return the result of a specific
+    //                         arithmetic operation (+ - * /)
 
-    text_t firstName = "Keike";
-    number_t age = 21;
+    int students = 20;
 
-    std::cout << firstName << "\n";
-    std::cout << age << "\n";
+    //students = students + 1;
+    students+=1;
+    students++;
+
+    //students = students - 2;
+    students-=2;
+
+    //students = students * 2;
+    students*=2;
+
+    //students = students / 3;
+    students/=3;
+
+    std::cout << students;
+
+    //parenthesis ()
+    //multiplication * & division /
+    //addition + & subtraction -
+
+    int result = 6 - (5 + 4) * 3 / 2;
+    std::cout << result;
 
     return 0;
 }
