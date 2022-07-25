800fbc4ebeef56b0e2d9f7b5a1bca012af83ab93
ArmZaDev <arztv001@gmail.com>
2022-07-25 20:31:35 +0700

    User input

--- test.cpp
+++ test.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
+#include <string>
 
 int main(){
 
-    // type conversion = conversion a value of one data type to another
-    //                   Implicit = automatic
-    //                   Explicit = Precede value with new data type (int)
-    
-    int correct = 8;
-    int questions = 10;
-    double score = correct/(double)questions * 100;
+    // cout << (insertion operator)
+    // cin >> (extraction operator)
 
-    std::cout << score << "%";
-    
-    /*
-    double x = (int)3.14;
-    std::cout << x;
-    */
+    std::string name;
+    int age;
+
+    std::cout << "What's your age?: ";
+    std::cin >> age;
+
+    std::cout << "What's your full name?: ";
+    std::getline(std::cin >> std::ws, name);
+
+    std::cout << "Hello " << name << "\n";
+    std::cout << "You are " << age << " years old" << "\n";
 
     return 0;
 }
