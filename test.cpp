d0978edf264a2a8e1944ee6839cd49b94056c525
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-07-26 13:04:33 +0700

    Console calculator program

--- test.cpp
+++ test.cpp
@@ -2,54 +2,43 @@
 
 
 int main(){
-    //switch = alternetive to sunig many "else if" statements
-    //         compare one value against macching cases
+    char op;
+    double num1 ,num2;
+    double result;
 
-    int month;
+    std::cout << "********** CALCULATOR **********\n";
 
-    std::cout << "Enter the month (1-12): ";
-    std::cin >> month;
+    std::cout << "Enter num #1: ";
+    std::cin >> num1;
 
-    switch(month){
-        case 1:
-            std::cout << "It is January";
+    std::cout << "Enter either (+ - * /): ";
+    std::cin >> op;
+
+    std::cout << "Enter num #2: ";
+    std::cin >> num2;
+
+    switch(op){
+        case '+':
+            result = num1 + num2;
+            std::cout << "result: " << result << '\n';
             break;
-        case 2:
-            std::cout << "It is February";
+        case '-':
+            result = num1 - num2;
+            std::cout << "result: " << result << '\n';
             break;
-        case 3:
-            std::cout << "It is March";
+        case '*':
+            result = num1 * num2;
+            std::cout << "result: " << result << '\n';
             break;
-        case 4:
-            std::cout << "It is April";
-            break;
-        case 5:
-            std::cout << "It is May";
-            break;
-        case 6:
-            std::cout << "It is June";
-            break;
-        case 7:
-            std::cout << "It is July";
-            break;
-        case 8:
-            std::cout << "It is August";
-            break;
-        case 9:
-            std::cout << "It is September";
-            break;
-        case 10:
-            std::cout << "It is October";
-            break;
-        case 11:
-            std::cout << "It is November";
-            break;
-        case 12:
-            std::cout << "It is December";
+        case '/':
+            result = num1 / num2;
+            std::cout << "result: " << result << '\n';
             break;
         default:
-            std::cout << "Please enter in only numbers (1-12)";
+            std::cout << "That wasn't a valid response";
     }
+
+    std::cout << "********************************\n";
 
     return 0;
 }
