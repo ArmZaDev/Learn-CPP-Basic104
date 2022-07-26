57fde86ef5fd3cc4266ea0f9c82a14d1f73c6d54
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-07-26 14:44:43 +0700

    Number guessing game

--- test.cpp
+++ test.cpp
@@ -3,21 +3,31 @@
 
 int main(){
 
-    srand(time(0));
-    int randNum = rand() % 5 + 1;
+    int num;
+    int guess;
+    int tries;
 
-    switch(randNum){
-        case 1: std::cout << "You win a bumper sticker!\n";
-                break;
-        case 2: std::cout << "You win a t-shirt!\n";
-                break;
-        case 3: std::cout << "You win a free lunch!\n";
-                break;
-        case 4: std::cout << "You win a gift card!\n";
-                break;
-        case 5: std::cout << "You win a concert tckets!\n";
-                break;
-    }
+    srand(time(NULL));
+    num = (rand() % 10) + 1;
+
+    std::cout << "****** NUMBER GUESSING GAME ******\n";
+
+    do{
+        std::cout << "Enter a guess between (1-10): ";
+        std::cin >> guess;
+        tries++;
+
+        if(guess > num){
+            std::cout << "Too high!\n";
+        }
+        else if (guess < num){
+            std::cout << "Too low!\n";
+        }
+        else{
+            std::cout << "CURRECT! # of tries: " << tries << '\n';
+        }
+
+    }while(guess != num);
 
     return 0;
 }
