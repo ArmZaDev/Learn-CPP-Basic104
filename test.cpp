7e9dc3ece356eecb313cb8633931327728223e76
ArmZaDev <arztv001@gmail.com>
2022-07-26 09:42:51 +0700

    Useful math related functions

--- test.cpp
+++ test.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
-#include <string>
+#include <cmath>
 
 int main(){
 
-    // cout << (insertion operator)
-    // cin >> (extraction operator)
+    double x = 3;
+    double y = 4;
+    double z;
 
-    std::string name;
-    int age;
+    //z = std::max(x ,y);
+    //z =std::min(x, y);
+    //z = pow(2, 4);
+    //z = sqrt(9);
+    //z = abs(-3);
+    //z = round(3.5);
+    //z = ceil(3.1);
+    z = floor(3.6);
 
-    std::cout << "What's your age?: ";
-    std::cin >> age;
-
-    std::cout << "What's your full name?: ";
-    std::getline(std::cin >> std::ws, name);
-
-    std::cout << "Hello " << name << "\n";
-    std::cout << "You are " << age << " years old" << "\n";
+    std::cout << z;
 
     return 0;
 }
