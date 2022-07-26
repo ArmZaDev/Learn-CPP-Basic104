96bc7bd50953289e0f8188be9bd1e6a38e2bdac6
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-07-26 14:13:43 +0700

    Do while loops

--- test.cpp
+++ test.cpp
@@ -3,14 +3,18 @@
 
 int main(){
     
-    std::string name;
+    // do while loop = do some block of code first,
+    //                 THEN repeat again if condition is true
 
-    while(name.empty()){
-        std::cout << "Enter your name: ";
-        std::getline(std::cin, name);
-    }
-    
-    std::cout << "Hello " << name;
+    int number ;
+
+    do{
+        std::cout << "Enter a positive #: ";
+        std::cin >> number;
+        
+    }while (number < 0);
+
+    std::cout << "The # is: " << number;
 
     return 0;
 }
