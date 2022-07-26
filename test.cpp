fcf2f3c5561d2c7d7c257b2983712dbfd2ae35aa
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-07-26 16:42:40 +0700

    Overloaded functions

--- test.cpp
+++ test.cpp
@@ -1,39 +1,22 @@
 #include <iostream>
-#include <string>
 
-double square(double length);
-double cube(double length);
-std::string concatString(std::string string1, std::string string2);
+void bakePizza();
+void bakePizza(std::string topping1);
+void bakePizza(std::string topping1, std::string toppnig2);
 
 int main(){
 
-    // return = return a value back to the spot
-    //          where you called the encompassing function
+    bakePizza("pepperoni", "mushroom");
 
-    std::string firstName = "Arm";
-    std::string lastName = "Za";
-    std::string fullName =concatString(firstName, lastName);
-
-    std::cout << "Hello " << fullName;
-
-/*
-    double length = 6.0;
-    double area = square(length);
-    double volume = cube(length);
-
-    std::cout << "Arae: " << area << "cm^2\n";
-    std::cout << "Volume: " << volume << "cm^3\n";
-*/
     return 0;
 }
 
-double square(double length){
-    return length * length;
+void bakePizza(){
+    std::cout << "Here is your pizza!\n";
 }
-double cube(double length){
-    return length * length * length;
+void bakePizza(std::string topping1){
+    std::cout << "Here is your " << topping1 << " pizza!\n";
 }
-
-std::string concatString(std::string string1, std::string string2){
-    return string1 + " " + string2;
+void bakePizza(std::string topping1, std::string toppnig2){
+    std::cout << "Here is your " << topping1 << " and " << toppnig2 << '\n';
 }
