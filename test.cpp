cd6c99428f1b75f372c352f85b6b439dbcdc7853
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-07-26 12:53:59 +0700

    Switch statement

--- test.cpp
+++ test.cpp
@@ -2,27 +2,54 @@
 
 
 int main(){
-    //If statements = do something if a condition is true.
-    //                if not, then don't do it.
+    //switch = alternetive to sunig many "else if" statements
+    //         compare one value against macching cases
 
-    int age;
+    int month;
 
-    std::cout << "Enter your age: ";
-    std::cin >> age;
+    std::cout << "Enter the month (1-12): ";
+    std::cin >> month;
 
-    if (age >= 18){
-        std::cout << "Welcome to the site!";
+    switch(month){
+        case 1:
+            std::cout << "It is January";
+            break;
+        case 2:
+            std::cout << "It is February";
+            break;
+        case 3:
+            std::cout << "It is March";
+            break;
+        case 4:
+            std::cout << "It is April";
+            break;
+        case 5:
+            std::cout << "It is May";
+            break;
+        case 6:
+            std::cout << "It is June";
+            break;
+        case 7:
+            std::cout << "It is July";
+            break;
+        case 8:
+            std::cout << "It is August";
+            break;
+        case 9:
+            std::cout << "It is September";
+            break;
+        case 10:
+            std::cout << "It is October";
+            break;
+        case 11:
+            std::cout << "It is November";
+            break;
+        case 12:
+            std::cout << "It is December";
+            break;
+        default:
+            std::cout << "Please enter in only numbers (1-12)";
     }
-    else if (age < 0){
-        std::cout << "You heven't been born yet!";
-    }
-    else if (age >= 45){
-        std::cout << "You are too old to enter this yet!";
-    }
-    else{
-        std::cout << "You are not old enough to enter!";
-    }
-    
 
     return 0;
 }
