aacbe750a203ba416c9ec0f00756684fa8369659
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-07-26 14:37:56 +0700

    Random event generator

--- test.cpp
+++ test.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
-
+#include <ctime>
 
 int main(){
 
-    // pseudo-random = NOT truly random (but close)
+    srand(time(0));
+    int randNum = rand() % 5 + 1;
 
-    srand(time(NULL));
-
-    int num = (rand() % 6) + 1;
-    int num1 = (rand() % 6) + 1;
-    int num2 = (rand() % 6) + 1;
-
-    std::cout << num << '\n';
-    std::cout << num1 << '\n';
-    std::cout << num2 << '\n';
+    switch(randNum){
+        case 1: std::cout << "You win a bumper sticker!\n";
+                break;
+        case 2: std::cout << "You win a t-shirt!\n";
+                break;
+        case 3: std::cout << "You win a free lunch!\n";
+                break;
+        case 4: std::cout << "You win a gift card!\n";
+                break;
+        case 5: std::cout << "You win a concert tckets!\n";
+                break;
+    }
 
     return 0;
 }
