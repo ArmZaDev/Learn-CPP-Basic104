94587798d1f012bf590c88f65a37e6af9b87fc18
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-07-26 09:50:15 +0700

    Hypotenuse calculator practice program

--- test.cpp
+++ test.cpp
@@ -3,20 +3,24 @@
 
 int main(){
 
-    double x = 3;
-    double y = 4;
-    double z;
+    double a;
+    double b;
+    double c;
 
-    //z = std::max(x ,y);
-    //z =std::min(x, y);
-    //z = pow(2, 4);
-    //z = sqrt(9);
-    //z = abs(-3);
-    //z = round(3.5);
-    //z = ceil(3.1);
-    z = floor(3.6);
+    std::cout << "Enter side A: ";
+    std::cin >> a;
 
-    std::cout << z;
+    std::cout << "Enter size B: ";
+    std::cin >> b;
+   
+    c = sqrt(pow(a, 2) + pow(b, 2));
+
+    std::cout << "side C: " << c;
+    /*
+    a = pow(a, 2);
+    b = pow(b, 2);
+    c = sqrt(a + b);
+    */
 
     return 0;
 }
