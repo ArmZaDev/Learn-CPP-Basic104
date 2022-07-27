d6b9d1afb665ef09d80626b98769890b58ca3668
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-07-27 13:39:59 +0700

    Arrays

--- test.cpp
+++ test.cpp
@@ -1,106 +1,21 @@
 #include <iostream>
-#include <ctime>
-
-char getUserChoice();
-char getComputerChoice();
-void showChoice(char choice);
-void chooseWinner(char player, char computer);
 
 int main(){
 
-    char player;
-    char computer;
-
-    player = getUserChoice();
-    std::cout << "You choice: ";
-    showChoice(player);
-
-    computer = getComputerChoice();
-    std::cout << "Computer's choice: ";
-    showChoice(computer);
-
-    chooseWinner(player, computer);
-
-    return 0;
-}
-
-char getUserChoice(){
-
-    char player;
-    std::cout << "Rock-Paper-Scissors Game!\n";
-    do{         
-        std::cout << "*************************\n";
-        std::cout << "'r' for rock\n";
-        std::cout << "'p' for paper\n";
-        std::cout << "'s' for scissors\n";
-        std::cin >> player;
-
-    }while(player != 'r' && player != 'p' && player != 's');
-        
-    return player;
-}
-char getComputerChoice(){
-
-    srand(time(0));
-    int num = rand() % 3 + 1;
-
-    switch(num){
-        case 1: return 'r';
-                break;
-        case 2: return 'p';
-                break;
-        case 3: return 's';
-                break;
-    }
+    // array = a data structure that can hold multiple values
+    //         values are accessed by an index number
+    //         "kind of like a variable that holds miltiple values"
+    
+    //std::string cars[] = {"Corvertte", "Mustang", "camry"};
+    std::string cars[3];
+    
+    cars[0] = "Camaro";
+    cars[1] = "Tesla";
+    cars[2] = "Audi";
+    
+    std::cout << cars[0] << '\n';
+    std::cout << cars[1] << '\n';
+    std::cout << cars[2] << '\n';
 
     return 0;
-}
-void showChoice(char choice){
-
-    switch(choice){
-        case 'r': std::cout << "Rock\n";
-                  break;
-        case 'p': std::cout << "Paper\n";
-                  break;
-        case 's': std::cout << "Scissors\n";
-                  break;
-    }
-
-}
-void chooseWinner(char player, char computer){
-
-    switch(player){
-        case 'r':   if(computer == 'r'){
-                        std::cout << "It's a tie!\n";
-                    }
-                    else if(computer == 'p'){
-                        std::cout << "You lose!\n";
-                    }
-                    else{
-                        std::cout << "You win!\n";
-                    }
-                    break;
-        case 'p':   if(computer == 'p'){
-                        std::cout << "It's a tie!\n";
-                    }
-                    else if(computer == 's'){
-                        std::cout << "You lose!\n";
-                    }
-                    else{
-                        std::cout << "You win!\n";
-                    }
-                    break;
-        case 's':   if(computer == 's'){
-                        std::cout << "It's a tie!\n";
-                    }
-                    else if(computer == 'r'){
-                        std::cout << "You lose!\n";
-                    }
-                    else{
-                        std::cout << "You win!\n";
-                    }
-                    break;
-
-    }
-
 }
