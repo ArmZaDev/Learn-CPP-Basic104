d10b7254ecfb6140477640dd020ee9058e0ae384
ArmZaDev <arztv001@gmail.com>
2022-07-27 17:06:50 +0700

    Fill() function

--- test.cpp
+++ test.cpp
@@ -1,35 +1,20 @@
 #include <iostream>
 
-void sort(int array[], int size);
 
 int main(){
 
-    int array[] = {10, 1, 9, 2, 8, 3, 7, 4, 6, 5};
-    int size = sizeof(array)/sizeof(array[0]);
+    // fill() = Filla range of elements with a secified value
+    //          fill(begin, end, value)
 
-    sort(array, size);
+    const int SIZE = 10;
+    std::string foods[SIZE];
 
-    for(int element : array){
-        std::cout << element << " ";
-    }    
+    fill(foods, foods + (SIZE / 2), "pizza");
+    fill(foods + (SIZE / 2), foods + SIZE, "hamburgers");   
+
+    for(std::string food : foods){
+        std::cout << food << '\n';
+    }
 
     return 0;
 }
-
-void sort(int array[], int size){
-
-    int temp;
-
-    for(int i = 0; i < size - 1; i++){ // size = 9, i = 1
-        for(int j = 0; j < size - i - 1; j++){ //size = 8, j = 1
-            if(array[j] > array[j + 1]){
-                temp = array[j];          // 10
-                array[j] = array[j + 1];  // 5
-                array[j + 1] = temp;      // 10
-            }
-        }
-    }
-
-}//      1 9  2  8 3 7  4 6  5 10
-
-// index 0 1  2  3 4 5  6 7  8 9
