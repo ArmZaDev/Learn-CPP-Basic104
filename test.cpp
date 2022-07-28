3bac8ffb375f05a98b2018c653fcf45ea931b4fc
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-07-28 16:13:54 +0700

    Recursion

--- test.cpp
+++ test.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
 
+int factorial(int num);
+
 int main(){
     
-    char *pGrades = NULL;
-    int size;
-
-    std::cout << "How many grades to enter in?: ";
-    std::cin >> size;
-
-    pGrades = new char[size];
-
-    for(int i = 0; i < size; i++){
-        std::cout << "Enter grade #" << i + 1 << ": ";
-        std::cin >> pGrades[i];
-    }
-
-    for(int i = 0; i < size; i++){
-        std::cout << pGrades[i] << " ";
-    }
-
-    delete[] pGrades;
+    std::cout << factorial(10);
 
     return 0;
+}
+
+int factorial(int num){
+
+    if(num > 1){
+        return num * factorial(num - 1);
+    }
+    else{
+        return 1;
+    }
+
+    /*
+    int result = 1;
+    for(int i = 1; i <= num; i++){ 
+        result = result * i;
+    }
+    return result;
+    */
 }
