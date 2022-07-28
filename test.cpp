a684a7fd9325e29f0940f685cbb0a402459ce667
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-07-28 16:46:49 +0700

    Structs

--- test.cpp
+++ test.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
-template <typename T, typename U>
-auto max(T x, U y){
-    return (x > y) ? x : y;
-}
-/*
-int max(int x, int y){
-    return (x > y) ? x : y;
-}
 
-double max(double x, double y){
-    return (x > y) ? x : y;
-}
-
-char max(char x, char y){
-    return (x > y) ? x : y;
-}*/
+struct student{
+    std::string name;
+    double gpa;
+    bool enrolled = true;
+};
 
 int main(){
     
-    std::cout << max(1, 2.1) << '\n';
+    student student1;
+    student1.name = "Naruto";
+    student1.gpa = 3.2;
+
+    std::cout << student1.name << '\n';
+    std::cout << student1.gpa << '\n';
+    std::cout << student1.enrolled << '\n';
 
     return 0;
 }
