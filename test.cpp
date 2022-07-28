0811ead1e8ecb3f5f87fe0bc00554d03a95db7e4
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-07-28 13:54:29 +0700

    Pointers

--- test.cpp
+++ test.cpp
@@ -1,49 +1,18 @@
 #include <iostream>
 
-int getDigit(const int number);
-int sumOddDigits(const std::string cardNumber);
-int sumEvenDigits(const std::string cardNumber);
-
 int main(){
+    
+    std::string name = "Naruto";
+    int age = 33;
+    std::string freePizza[5] = {"pizza1", "pizza2", "pizza3", "pizza4", "pizza5"};
+    
+    std::string *pName = &name;
+    int *pAge = &age;
+    std::string *pFreePizza = freePizza;
 
-    std::string cardNumber;
-    int result = 0;
-
-    std::cout << "Enter a credit card #: ";
-    std::cin >> cardNumber;
-
-    result = sumEvenDigits(cardNumber) + sumOddDigits(cardNumber);
-
-    if(result % 10 == 0){
-        std::cout << cardNumber<< " is valid";
-    }
-    else{
-        std::cout << cardNumber << " is not valid";
-    }
+    std::cout << *pName << '\n';
+    std::cout << *pAge << '\n';
+    std::cout << *pFreePizza << '\n';
 
     return 0;
-}
-
-int getDigit(const int number){
-    
-    return number % 10 + (number / 10 % 10);
-}
-int sumOddDigits(const std::string cardNumber){
-
-    int sum = 0;
-
-    for(int i = cardNumber.size() - 1; i >= 0; i-=2){
-        sum += cardNumber[i] - '0';
-    }
-    return sum;
-} 
-int sumEvenDigits(const std::string cardNumber){
-
-    int sum = 0;
-
-    for(int i = cardNumber.size() - 2; i >= 0; i-=2){
-        sum += getDigit((cardNumber[i] - '0') * 2);
-    }
-
-    return sum;
 }
