4c68f26fd667e6b67dac32dd57e6120a81278f75
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-07-28 17:27:26 +0700

    Constructors

--- test.cpp
+++ test.cpp
@@ -1,35 +1,25 @@
 #include <iostream>
 
-class Human{
+class Student{
     public:
         std::string name;
-        std::string occupation;
         int age;
-
-        void eat(){
-            std::cout << "This person is eating\n";
-        }
-        void drink(){
-            std::cout << "This person is drinking\n";
-        }
-        void sleep(){
-            std::cout << "This person is sleeping\n";
-        }
+        double gpa;
+    Student(std::string name, int age, double gpa){
+        this->name = name;
+        this->age = age;
+        this->gpa = gpa;
+    }
+       
 };
 
 int main(){
     
-    Human human1;
+    Student student1("Spongebob", 25, 3.2);
 
-    human1.name = "Tommy";
-    human1.occupation = "bookmaker";
-    human1.age = 35;
-
-    std::cout << human1.name << '\n';
-    std::cout << human1.occupation << '\n';
-    std::cout << human1.age << '\n';
-
-    human1.drink();
+    std::cout << student1.name << '\n';
+    std::cout << student1.age << '\n';
+    std::cout << student1.gpa << '\n';
 
     return 0;
 }
